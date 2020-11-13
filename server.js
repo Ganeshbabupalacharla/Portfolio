1bd3b10eab1bd4b207f4ea4561dc46ebf9a624df
Ganesh <ganeshpalacharla@outlook.com>
2020-11-13 06:49:23 -0500

    one more

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use('/api/form',require('./Routes/api/form'));
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('/client/build'));
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname,'/client/build/index.html'));
   });
 };
 
