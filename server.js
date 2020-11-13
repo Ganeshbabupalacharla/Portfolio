33f76d5875b58eb9d75b2e1e63734ddcebdbcd5a
Ganesh <ganeshpalacharla@outlook.com>
2020-11-13 06:25:36 -0500

    hello

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express=require('express');
 const connectDB=require('./config/db');
 const bodyParser=require('body-parser');
 const path=require('path');
-const form=require('./Routes/api/form');
 const app=express();
 connectDB();
 
@@ -17,7 +16,7 @@ if(process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'));
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
   });
 };
 
