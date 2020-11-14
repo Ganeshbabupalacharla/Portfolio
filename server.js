f41548b5882c779011eb7696392459b0634e3d42
Ganesh <ganeshpalacharla@outlook.com>
2020-11-13 22:51:02 -0500

    Deploying

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express=require('express');
 const connectDB=require('./config/db');
-const bodyParser=require('body-parser');
 const path=require('path');
 const app=express();
 connectDB();
 
 //bodyparser middleware
-
-app.use(bodyParser.json());
+const clientAppDirectory=path.join(__dirname, '/client', 'build');
+app.use(express.json());
 //use routes
 app.use('/api/form',require('./Routes/api/form'));
 
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static('/client/build'));
+  app.use(express.static(clientAppDirectory ));
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/client/build/index.html'));
+    res.sendFile(path.join(clientAppDirectory,'index.html'));
   });
 };
 
