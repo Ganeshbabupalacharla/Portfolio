086d4b085581c8666131d9f3889f8643a620ac72
Ganesh <ganeshpalacharla@outlook.com>
2020-11-13 23:38:54 -0500

    push

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const app=express();
 connectDB();
 
 //bodyparser middleware
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 //use routes
 app.use('/api/form',require('./Routes/api/form'));
 
 //serve static assets in production
-if(process.env.NODE_ENV === 'production'){
+
   //set static folder
   app.get('*', (request, response) => {
     return response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-};
 
 const port=process.env.PORT || 5000;
 
