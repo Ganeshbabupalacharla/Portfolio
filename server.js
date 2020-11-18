ea6140f25c82d2065d148b835b4833e82a360de8
Ganesh <ganeshpalacharla@outlook.com>
2020-11-18 12:26:16 -0500

    deploy

--- server.js
+++ server.js
@@ -8,17 +8,23 @@ connectDB();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, './public','build')));
 //use routes
 app.use('/api/form',require('./Routes/api/form'));
 
 //serve static assets in production
 
   //set static folder
-  app.get('*', (request, response) => {
-    return response.sendFile(path.join(__dirname, '/client/build','index.html'));
+app.get('/*', (request, response) => {
+    return response.sendFile(path.join(__dirname, './public/build','index.html'));
 });
 
 const port=process.env.PORT || 5000;
 
 app.listen(port,()=>console.log(`server started on port ${port}`));
+
+// "server":"nodemon server.js",
+//     "client":"npm start --prefix client",
+//     "client-install":"npm install --prefix client",
+//     "dev":"concurrently \"npm run server\" \"npm run client\"",
+//     "heroku-postbuild": "cd client --only=dev && npm install && npm run build"
